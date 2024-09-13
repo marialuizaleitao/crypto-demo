055872515a31ff6d6590cd593ab5b9bd0a197cec
Maria Leitão <88951059+marialuizaleitao@users.noreply.github.com>
2024-09-13 13:39:18 +0000

    feat: add ndex.js to run files

---
+++ index.js
@@ -0,0 +1,10 @@
+// Importando e executando o arquivo de criptografia assimétrica RSA
+console.log("=== Executando Criptografia Assimétrica (RSA) ===");
+require('./asymmetric-encryption-rsa.js');
+
+// Separador de saída para clareza no console
+console.log("\n============================================\n");
+
+// Importando e executando o arquivo de criptografia simétrica AES
+console.log("=== Executando Criptografia Simétrica (AES) ===");
+require('./symmetric-encryption-aes.js');
